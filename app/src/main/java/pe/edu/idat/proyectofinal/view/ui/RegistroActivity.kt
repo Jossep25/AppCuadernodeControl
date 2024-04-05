75b04d6f20d157c2a09fa6619f4e61a4d74c8c19
Jossep25 <lolitoscomor@gmail.com>
2024-04-04 19:01:28 -0500

    Inicio del proyecto final Cuaderno de Control

---
+++ app/src/main/java/pe/edu/idat/proyectofinal/view/ui/RegistroActivity.kt
@@ -0,0 +1,12 @@
+package pe.edu.idat.proyectofinal.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pe.edu.idat.proyectofinal.R
+
+class RegistroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+    }
+}
