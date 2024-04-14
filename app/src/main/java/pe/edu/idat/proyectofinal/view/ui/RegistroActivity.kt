aa84a02bbc7ff86391760da1473f716ceb365b13
Steven Felipa <114965934+steve13XD@users.noreply.github.com>
2024-04-14 17:39:22 -0500

    estilos cambiados y se agrega calendario

--- app/src/main/java/pe/edu/idat/proyectofinal/view/ui/RegistroActivity.kt
+++ app/src/main/java/pe/edu/idat/proyectofinal/view/ui/RegistroActivity.kt
@@ -1,21 +1,29 @@
 package pe.edu.idat.proyectofinal.view.ui
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import pe.edu.idat.proyectofinal.retrofit.response.RegistrarResponse
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.DatePicker
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.textfield.TextInputEditText
 import pe.edu.idat.proyectofinal.R
 import pe.edu.idat.proyectofinal.databinding.ActivityRegistroBinding
 import pe.edu.idat.proyectofinal.util.AppMensaje
 import pe.edu.idat.proyectofinal.util.TipoMensaje
 import pe.edu.idat.proyectofinal.viewmodel.AuthViewModel
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class RegistroActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityRegistroBinding
     private lateinit var authViewModel: AuthViewModel
+    private lateinit var edFecha: TextInputEditText
+    private var cal = Calendar.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegistroBinding.inflate(layoutInflater)
@@ -23,10 +31,43 @@ class RegistroActivity : AppCompatActivity(), View.OnClickListener {
         authViewModel = ViewModelProvider(this).get(AuthViewModel::class.java)
         binding.btncancelar.setOnClickListener(this)
         binding.btnregistrar.setOnClickListener(this)
+
+        edFecha = binding.edfecha
+        edFecha.setOnClickListener{
+            mostrarDatePicker()
+        }
         authViewModel.registrarResponse.observe(this, Observer {
                 response -> obtenerDatosRegistro(response)
         })
     }
+
+    private fun mostrarDatePicker() {
+        val datePickerDialog = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { _: DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                actualizarFechaEnEditText()
+            },
+            cal.get(Calendar.YEAR),
+            cal.get(Calendar.MONTH),
+            cal.get(Calendar.DAY_OF_MONTH)
+        )
+
+        // Configuramos el DatePickerDialog para que solo muestre fechas anteriores
+        datePickerDialog.datePicker.maxDate = System.currentTimeMillis()
+
+        datePickerDialog.show()
+    }
+
+    private fun actualizarFechaEnEditText() {
+        val formato = "yyyy-MM-dd"
+        val sdf = SimpleDateFormat(formato, Locale.getDefault())
+        edFecha.setText(sdf.format(cal.time))
+    }
+
+
     private fun obtenerDatosRegistro(response: RegistrarResponse) {
         binding.btncancelar.isEnabled = true
         binding.btnregistrar.isEnabled = true
